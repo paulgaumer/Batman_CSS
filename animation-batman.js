d5ebe8a0e26d676110e32056b5bfa5af8f9748b5
paulgaumer <mitsudomoe8@gmail.com>
2016-03-23 19:58:00 +0900

    First commit

---
+++ animation-batman.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$(".flash").click(function() {
+		$(".eyes").toggleClass("laser");
+	});
+
+	$(".flash").click(function() {
+		$(".dialogue").toggle();
+	});
+
+});
